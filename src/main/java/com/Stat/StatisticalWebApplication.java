7a88c1854faf25175daea2d2adfb83aee41431c3
peytonhobson <peyton.hobson1@gmail.com>
2021-12-19 16:23:16 -0800

    Outlined packages

---
+++ src/main/java/com/Stat/StatisticalWebApplication.java
@@ -0,0 +1,13 @@
+package com.Stat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticalWebApplication.class, args);
+	}
+
+}
